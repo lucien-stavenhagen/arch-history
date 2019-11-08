fd0aa11c8a33e08b92acec0216610705dfc08722
luciens6669 <lstavenhagen@hotmail.com>
2019-11-08 17:43:32 -0600

    add devtool source-map to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 module.exports = {
+  devtool: "source-map",
   entry: {
     orders: ["@babel/polyfill", "./src/orders.js"],
     allorders: ["@babel/polyfill", "./src/allorders.js"]
